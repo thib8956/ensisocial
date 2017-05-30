ee066733d1629132a1489795529c3ead357d34c9
François Straebler <francois.straebler42@gmail.com>
2017-05-30 17:20:17 +0200

    bar de recherche

--- page_membre.php
+++ page_membre.php
@@ -3,10 +3,20 @@ session_start();
 
 $title=$_SESSION['username'];
 include("header.php");
+?>
 
-if (isset($_SESSION['username'])){
-	echo '<p>Bonjour, '.$_SESSION['username'].'</p>';
-}
+<form method="post" action="userSearch.php">
+    <p>
+        <label for "rechercheBar"> Recherche : </label>
+        <input type="text" name="searchBar" placeholder="recherche..." />
+    </p>
+    <p>
+        <input type="submit" value="Rechercher" name="search" />
+    </p>
+</form>
+
+<?php
+echo '<p>Bonjour, '.$_SESSION['username'].'</p>';
 
 echo '<a href="disconnection.php">Déconnexion</a>';
 ?>
