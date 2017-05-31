61110f0ac145dbc65835022c84fbd7547b0ee992
François Straebler <francois.straebler42@gmail.com>
2017-05-31 17:42:06 +0200

    un bon debut

--- inc/navbar.php
+++ inc/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default">
 	<ul class="nav navbar-nav">
-		<?php if (!isset($_SESSION['username'])){
+		<?php if (!isset($_SESSION['email'])){
 			echo '<li><a href="inscription.php" title="inscription">Inscription</a></li>';
 			echo '<li><a href="connection.php" title="connexion">Connexion</a></li>';
 		} else { ?>
@@ -11,9 +11,9 @@
             echo '<li><a href="">Groupes</a></li>';
             echo '<li><a href="">Contacts récents</a></li>';
 			?>
-			<form class="navbar-form navbar-left" method="post" action="userSearch.php">
+			<form class="navbar-form navbar-left" method="post">  <!-- mettre action="truc.php" -->
 				<div class="form-group">
-					<input type="text" class="form-control" name="searchBar" placeholder="Search">
+					<input type="text" id="searchBar" class="form-control" name="searchBar" placeholder="Search" autocomplete="off">
 				</div>
 				<button type="submit" class="btn btn-default" name="search">Submit</button>
 			</form>
--- userSearch.php
+++ userSearch.php
@@ -1,26 +1,28 @@
 <?php
-$title="Recherche";
-include('inc/header.php');
+    try {
+		$db = new PDO("mysql:host=localhost;dbname=ensisocial;charset=utf8", "root", "");
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch (Exception $e) {
+		die('Error:'.$e->getMessage());
+	}
 
-if(isset($_POST['search'])){
-	if (!empty($_POST['searchBar'])) {
-        $wordList = explode(" ", $_POST['searchBar'], PHP_INT_MAX);
-        for ($i = 0; $i < count($wordList); $i++) {
-            $req = $db->query('Select * FROM users WHERE firstname LIKE "%'.$wordList[$i].'%"');
-            $rep = $req->fetch();
-            if(!empty($rep)) {
-                 echo "<p>".$rep['firstname']." ".$rep['lastname']."</p>";
-            }
-            else {
-                $req = $db->query('Select * FROM users WHERE lastname LIKE "%'.$wordList[$i].'%"');
-                $rep = $req->fetch();
-                if(!empty($rep)) {
-                    echo "<p>".$rep['firstname']." ".$rep['lastname']."</p>";
-                }
-            }
-        }
+    $term = $_GET['term'];
+    $termm = $_GET['term'];
+
+    $requete = $db->prepare('SELECT * FROM users WHERE firstname LIKE :term OR lastname LIKE :termm'); // j'effectue ma requête SQL grâce au mot-clé LIKE
+
+    $requete->execute(array('term' => '%'.$term.'%',
+                             'termm' => '%'.$termm.'%'));
+
+    $array = array(); // on créé le tableau
+
+
+    while($donnee = $requete->fetch()) // on effectue une boucle pour obtenir les données
+
+    {
+        array_push($array, $donnee['firstname'].' '.$donnee['lastname']); // et on ajoute celles-ci à notre tableau
+
     }
-}
-
-include(inc/footer.php);
+    echo json_encode($array); // il n'y a plus qu'à convertir en JSON
 ?>
