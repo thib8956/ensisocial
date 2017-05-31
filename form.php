a94e4b05972c2a681ad930a4199d20f869e388b1
florian <florian.jaby@uha.fr>
2017-05-31 09:44:02 +0200

    Creation de la classe form

---
+++ form.php
@@ -0,0 +1,89 @@
+<?php
+
+
+class form{
+    private $data;
+    private $surround='div';
+    private $button;
+
+    /**
+     * form constructor.
+     * @param $data array regroupe tous les champs du formulaire
+     *        $button donne le texte afficher sur le boutton
+     */
+    public function __construct($data,$button){
+        $this->data=$data;
+        $this->button=$button;
+    }
+
+    /**
+     * @param $html
+     */
+    private function surround($html){
+        return "<{$this->surround} >{$html}</{$this->surround}>";
+    }
+
+    /**
+     * @param $index
+     * @return string
+     *     */
+    private function getValue($index){
+        return isset($this->data[$index]) ? $this->data[$index]:null;
+    }
+
+    /**
+     * @param $name
+     * @param $type
+     * @param $display
+     * @param $option
+     * @return string
+     */
+    public function inputsection($name, $type, $display, $option){
+
+        $section='<label for="'.$display.'" class="control-label"></label>
+						<select name="'.$name.'" class="form-control">';
+        foreach($option as $key =>$value){
+            $section = $section . '<option value = ' . $key . '>' . $option[$key] . '</option >';
+        }
+
+						$section=$section.'</select>';
+        return $this->surround($section);
+
+    }
+
+
+    /**
+     * @param $index
+     * @return string
+     */
+    private function getType($index){
+        return isset($this->data[$index]) ? gettype($this->data[îndex]):null;
+    }
+
+
+    /**
+     * @param $name
+     * @param $type
+     * @param $display
+     * @param bool $mandatory
+     * @return string
+     */
+    public function inputfield($name, $type, $display, $mandatory=false){
+        if ($mandatory==true) {
+            return $this->surround(
+                '<label for="'.$name.'" class="control-label">'.$display.'<span class="asteriskField">*</span></label>
+						<input type="'.$type.'" name="'.$name.'" class="form-control">');
+        }
+        return $this->surround(
+            '<label for="'.$name.'" class="control-label">'.$display.'</label>
+						<input type="'.$type.'" name="'.$name.'" class="form-control">');
+
+    }
+
+    /**
+     * @return string retourn la commande html pour créer le bouton du formulaire
+     */
+    public function submit($display){
+        return $this->surround('<input type="submit" value ="'.$display.'" name="'.$this->button.'" class="btn btn-primary">');
+    }
+}
--- inscription.php
+++ inscription.php
@@ -1,126 +1,30 @@
 <?php
+
 $title="Inscription";
 include("header.php");
+require "form.php";
+$form=new Form($_POST,"signin");
 ?>
 <div class="bootstrap-iso">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-6 col-sm-6 col-xs-12">
 				<form action="traitement.php" method="post">
-
-					<!-- username -->
-					<div class="form-group">
-						<label for="username" class="control-label">Nom d'utilisateur<span class="asteriskField">*</span></label>
-						<input type="text" name="username" class="form-control">
-					</div>
-					<!-- username / End -->
-
-					<!-- password -->
-					<div class="form-group">
-						<label for="password" class="control-label">Mot de Passe<span class="asteriskField">*</span></label>
-						<input type="password" name="password" class="form-control">
-						<span class="error-message"></span>
-					</div>
-					<!-- password / End -->
-
-					<!-- repassword -->
-					<div class="form-group">
-						<label for="repassword" class="control-label">Retapez votre Mot de Passe<span class="asteriskField">*</span></label>
-						<input type="password" name="repassword" class="form-control">
-						<span class="error-message"></span>
-					</div>
-					<!-- repassword / End -->
-
-					<!--firstname -->
-
-					<div class="form-group">
-						<label for="firstname" class="control-label">Prenom</label>
-						<input type="text" name="firstname" class="form-control">
-					</div>
-
-					<!-- firstame / End -->
-
-					<!-- secondname -->
-					<div class="form-group">
-						<label for="secondname" class="control-label"> Deuxième prenom </label>
-						<input type="text" name="secondname" class="form-control">
-					</div>
-
-					<!-- secondname / End -->
-
-					<!-- lastname -->
-					<div class="form-group">
-						<label for="lastname" class="control-label">Nom </label>
-						<input type="text" name="lastname" class="form-control">
-					</div>
-					<!-- lastname / End -->
-
-					<!-- address -->
-					<div class="form-group">
-						<label for="address" class="control-label">Adresse</label>
-						<input type="text" name="address" class="form-control">
-					</div>
-					<!-- address / End -->
-
-					<!-- zipcode -->
-					<div class="form-group">
-						<label for="zipcode" class="control-label">code postal</label>
-						<input type="text" name="zipcode" class="form-control">
-					</div>
-					<!-- zipcode / End -->
-
-					<!-- town -->
-					<div class="form-group">
-						<label for="town" class="control-label">Ville</label>
-						<input type="text" name="town" class="form-control">
-					</div>
-					<!-- town / End -->
-
-					<!-- birth -->
-					<div class="form-group">
-						<label for="birth" class="control-label">Date de naissance</label>
-						<input type="date" name="birth" class="form-control">
-					</div>
-					<!-- birth / End -->
-
-
-					<!-- email -->
-					<div class="form-group">
-						<label for="email" class="control-label">Email<span class="asteriskField">*</span></label>
-						<input type="email" name="email" class="form-control">
-					</div>
-					<!-- email / End -->
-
-					<!-- phone -->
-					<div class="form-group">
-						<label for="phone" class="control-label">Telephone </label>
-						<input type="text" name="phone" class="form-control">
-					</div>
-					<!-- phone / End -->
-
-					<!--formation>-->
-					<div class="form-group">
-						<label for="formation" class="control-label"></label>
-						<select name="formation" class="form-control">
-							<option value="IR">Informatique et réseau</option>
-							<option value="AS">Automatique et Système</option>
-							<option value="meca">Mecanique</option>
-							<option value="textile">Textile</option>
-							<option value="FIP">filière par alternance</option>
-						</select>
-					</div>
-					<!-- formation / End -->
-
-					<div class="form-group"> <!-- Submit button !-->
-						<input type="submit" value ="S'inscrire !" name="signin" class="btn btn-primary">
-					</div>
-
-				</form>
-			</div>
-		</div>
-	</div>
-</div>
-
 <?php
+    echo $form->inputfield("username","string","Nom d'utilisateur",true);
+    echo $form->inputfield("password","string","Mot de passe",true);
+    echo $form->inputfield("repassword","string","Retapez votre mot de passe",true);
+    echo $form->inputfield("firstname","string","prénom");
+    echo $form->inputfield("secondname","string","Deuxième prénom");
+    echo $form->inputfield("lastname","string","Nom");
+    echo $form->inputfield("address","string","Adresse");
+    echo $form->inputfield("zipcode","int","Code Postale");
+    echo $form->inputfield("town","string","Ville");
+    echo $form->inputfield("birth","date","Date de naissance");
+    echo $form->inputfield("email","email","Votre email",true);
+    echo $form->inputfield("phone","string","Téléphone");
+    echo $form->inputsection("formation","string","formation",array("IR" => "Informatique et réseau","AS" =>"AUtomatique et système" ,"meca" => "Mecanique","textile"=>"Textile","FIP" => "Filière par alternance"));
+    echo $form->submit("S'inscrire !");
+
 include("footer.php");
 ?>
