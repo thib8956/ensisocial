c7fa00479d888f33f355349d7be1a3ff4a3919b3
Bastien Geldreich <bastiengeldreich@MacBook-Air-de-Bastien-2.local>
2017-06-06 16:23:10 +0200

    profil sans style sans liens encore+ mail non indésirable pour free (toujours supprimé par les autres serveurs de messagerie)

--- profile.php
+++ profile.php
@@ -7,8 +7,18 @@ $title="Profil";
 include_once($_SERVER['DOCUMENT_ROOT'].'/ensisocial/inc/header.php');
 ?>
 
-<section id="profil">
-
+<section id="profil"> // un beau contour pour le tout avec un fond spécial
+    <div class="divprofil"><p>Image de profil <a>Modifier</a></p></div> //divprofil pour souligner
+    <div class="divprofil"><p>Mot de passe <a>Modifier</a></p></div>
+    <div class="divprofil"><p>Nom</p></div>
+    <div class="divprofil"><p>Prénom</p></div>
+    <div class="divprofil"><p>Filière</p></div>
+    <div class="divprofil"><p>Mail</p></div>
+    <div class="divprofil"><p>Date de naissance</p></div>
+    <div class="divprofil"><p>Adresse <a>Modifier</a></p></div>
+    <div class="divprofil"><p>Code postal <a>Modifier</a></p></div>
+    <div class="divprofil"><p>Ville <a>Modifier</a></p></div>
+    <div class="divprofil"><p>Image de profil <a>Modifier</a></p></div>
 </section>
 
 <?php
