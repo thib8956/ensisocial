71976a76e88c256ecc368188db57e7353b08388c
florian <florian.jaby@uha.fr>
2017-06-07 16:11:58 +0200

    ajout de like et dislike

--- js/memberconnected.js
+++ js/memberconnected.js
@@ -4,34 +4,43 @@ function writeInDiv(text){
 }
 var tmp = setInterval(function(){ refresh() }, 10000);
 
-function refresh()
-{
-    var xhr=null;
-    if (window.XMLHttpRequest) {
-        xhr = new XMLHttpRequest();
+function createRequestObject() {
+    var http;
+    if(window.XMLHttpRequest || window.ActiveXObject){
+        if(window.ActiveXObject){
+            try{
+                http= new ActiveXObject("Msxm12.XMLHTTP")
+            }catch(e){
+                http= new ActiveXObject("Microsoft.XMLHTTP")
+            }
+        }else {
+            http=new XMLHttpRequest();
+        }
+    } else  {
+       alert("Votre navigateur ne supporte pas l'objet XMLHttpRequest");
+    return null
     }
-    else if (window.ActiveXObject){
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xhr.open("GET", "/ensisocial/memberconnected.php", false);
+    return http;
+}
+
+
+function refresh() {
+    var xhr=createRequestObject();
+    xhr.open("GET", "memberconnected.php", false);
     xhr.send(null);
     writeInDiv(xhr.responseText);
 }
 
-$('#thumbs_up').click(function() {
-    // L'URL du fichier dans lequel tu appelles ta fonction
-    var url = '/thumbup.php';
-    $.post(url, function(data){
-        // Tu affiches le contenu dans ta div
-        $('.thumbup').html(data);
-    });
-});
+function clicup(num) {
+    http = createRequestObject();
+    http.open("POST", 'thumb.php', true);
+    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.send("num="+num+"&action=0");
+}
 
-$('#thumbs_down').click(function() {
-    // L'URL du fichier dans lequel tu appelles ta fonctio
-    var url = '/thumbdown.php';
-    $.post(url, function(data){
-        // Tu affiches le contenu dans ta div
-        $('.thumbdown').html(data);
-    });
-});
+function clicdown(num) {
+    http = createRequestObject();
+    http.open("POST", 'thumb.php', true);
+    http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    http.send("num="+num+"&action=1");
+}
