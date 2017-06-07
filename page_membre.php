79b76aea4f608f481d1c36847e7cb30f05924d73
François Straebler <francois.straebler42@gmail.com>
2017-06-07 15:29:10 +0200

    des commentaires du feu de dieu

--- page_membre.php
+++ page_membre.php
@@ -91,14 +91,12 @@ try {
 	</div>
 </div>
 <!-- Display newsfeed -->
-<<<<<<< HEAD
-<div class="newsfeed">
-<div class="col-sm-offset-2 col-md-10 test">
-=======
-<div class="col-sm-offset-2 col-md-9">
->>>>>>> e0debed4a5a30226126993355b45a2a803a03e2d
+<div class="newsfeedwrap">
+<div class="col-sm-offset-2 col-md-9 newsfeed">
 	<?php
+    $commId=0;
 	while ($publication=$stmt->fetch()){
+        $commId+=1;
 		$avatar = '/ensisocial/data/avatar/'.$publication['profile_pic'];
 		?>
 		<div class="panel panel-default">
@@ -125,7 +123,7 @@ try {
 					?>
 					<!-- Add a comment -->
 					<div class="input-group">
-						<form class="submitAjax" action="/ensisocial/comment_submit.php" method="post" accept-charset="utf-8">
+						<?php echo '<form id="comm'.$commId.'" class="submitAjax" action="/ensisocial/comment_submit.php" method="post" accept-charset="utf-8">' ?>
 							<input class="form-control" placeholder="Ajouter votre commentaire" type="text" name="add" autocomplete="off">
 							<?php echo '<input type="hidden" name="back" value='.$_SERVER['REQUEST_URI'].'>' ?>
 							<?php echo '<input name="post_id" type="hidden" value='.$publication['newsfeedid'].'>' ?>
@@ -137,5 +135,6 @@ try {
 		<?php
 		} // /while
 		echo '</div>'; /* /.col-sm-offset-2 .col-md-9 */
+        echo '</div>'; /* /.newsfeed */
 		include_once($_SERVER['DOCUMENT_ROOT'].'/ensisocial/inc/footer.php');
 		?>
