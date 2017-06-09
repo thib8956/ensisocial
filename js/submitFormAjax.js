0e672d2f8b2dcba6d5d1399bdb890db4faba2eaa
François Straebler <francois.straebler42@gmail.com>
2017-06-09 13:54:25 +0200

    j aime mettre mon penis, dans les yaourt

--- js/submitFormAjax.js
+++ js/submitFormAjax.js
@@ -65,4 +65,17 @@ $(document).ready(function() {
             }
         });
     });
+    
+    $('body').on('click', '.thumb', function(e) {
+        e.preventDefault(); // J'empêche le comportement par défaut du navigateur, c-à-d de soumettre le formulaire
+        var $this = $(this); // L'objet jQuery du formulaire
+        // Envoi de la requête HTTP en mode asynchrone
+        $.ajax({
+            url: $this.attr('href'), // Le nom du fichier indiqué dans le formulaire
+            data: $this.serialize(), // Je sérialise les données (j'envoie toutes les valeurs présentes dans le formulaire)
+            success: function(){
+                $(".newsfeedwrap").load(location.href + " .newsfeed");
+            }
+        });
+    });
 });
