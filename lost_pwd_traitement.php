292621c645233af324e097976edd93c9bdbcbd65
Bastien Geldreich <bastiengeldreich@MacBook-Air-de-Bastien-2.local>
2017-06-13 00:15:25 +0200

    changement d'ordre entre ssl et tls

--- lost_pwd_traitement.php
+++ lost_pwd_traitement.php
@@ -26,10 +26,10 @@ require 'phpmailer/PHPMailerAutoload.php';
         $req2= $db->prepare('UPDATE users SET password="'.$hashmdp.'" WHERE email = "'.$to.'"');
         $mail = new PHPMailer;
         $mail->isSMTP();
-        $mail->SMTPSecure = 'ssl';
+        $mail->SMTPSecure = 'tls';
         $mail->SMTPAuth = true;
         $mail->Host = 'smtp.gmail.com';
-        $mail->Port = 465;
+        $mail->Port = 587;
         $mail->Username = 'ensisocial@gmail.com';
         $mail->Password = 'motdepassedebg';
         $mail->setFrom('ensisocial@gmail.com');
@@ -50,11 +50,13 @@ require 'phpmailer/PHPMailerAutoload.php';
         //send the message, check for errors
         if (!$mail->send()) {
             //echo "ERROR: " . $mail->ErrorInfo;
-            $mail->SMTPSecure = 'tls';
-            $mail->Port = 587;
+            $mail->SMTPSecure = 'ssl';
+            $mail->Port = 465;
             if($mail->send()){
                 echo "<div>Mot de passe envoyé via TLS</div>";
                 $req2->execute();
+            } else {
+                echo $mail->ErrorInfo;
             }
         } else {
             echo "<div>Votre mot de passe a été envoyé à l'adresse ".$to."</div>";
